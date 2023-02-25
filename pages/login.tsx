d611d5aacea063cae73070a8dc1e0b106e60c0e1
tausifabid12 <tasifabid12@gmail.com>
2023-02-25 22:29:40 +0600

    login and signUp card updated

--- pages/login.tsx
+++ pages/login.tsx
@@ -74,7 +74,7 @@ const LogIn: React.FC = () => {
       </div>
 
       <div className="min-h-screen  py-6 flex flex-col justify-center sm:py-12">
-        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="relative py-3 sm:min-w-[470px] sm:max-w-xl sm:mx-auto">
           <div className="absolute inset-0 bg-transparent  lg:bg-gradient-to-r from-primary/30 to-primary lg:shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
           <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
             <div className="max-w-md mx-auto">
